77f5eff3fe52c3e990899fbcb6354c2e40c02a98
T14s <122837733+looonnng@users.noreply.github.com>
2024-04-26 00:19:33 -0500

    Create add card function

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // TODO : Add function to prevent/filter out duplicates
-// TODO : Add restrection/requirement for the user inputs, ie: prevent user from submitting empty fields
+// TODO : Prevent user from submitting read status only <-- user could submit form with only yes or no input
 
 const myLibrary = [];
 
@@ -8,21 +8,26 @@ const addBookBtn = document.querySelector("#addBook");
 const titleField = document.querySelector("#title");
 const authorField = document.querySelector("#author");
 const pagesField = document.querySelector("#pages");
+const cardsContainer = document.querySelector("#cards-container");
 
-addBookBtn.addEventListener("click", () => {
+addBookBtn.addEventListener("click", (e) => {
   const readStatus = document.querySelector(
     'input[name="read-status"]:checked'
   );
-  let book = new Book(
+
+  let newBook = new Book(
     titleField.value,
     authorField.value,
     pagesField.value,
     readStatus.value
   );
 
-  addBookToLibrary(book);
+  addBookToLibrary(newBook);
 
-  document.querySelector("form").reset();
+  document.querySelector("form").reset(); // Reset all fields after button is clicked
+
+  addCard(newBook);
+  e.preventDefault();
 });
 
 function Book(title, author, pages, readStatus) {
@@ -40,10 +45,45 @@ function Book(title, author, pages, readStatus) {
   };
 }
 
-let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-
 // book should be an object instance of Book constructor
 function addBookToLibrary(book) {
   myLibrary.push(book);
   console.log("book added");
 }
+
+let theGreatGatsby = new Book(
+  "The Great Gatsby",
+  "F. Scott Fitzgerald",
+  137,
+  "no"
+);
+let donQuixote = new Book("Don Quixote", "Miguel de Cervantes", 1072, "no");
+let toKillAMockingbird = new Book(
+  "To Kill a Mockingbird",
+  "Harper Lee",
+  281,
+  "yes"
+);
+
+myLibrary.push(theGreatGatsby);
+myLibrary.push(donQuixote);
+myLibrary.push(toKillAMockingbird);
+
+function addCard(book) {
+  const div = document.createElement("div");
+
+  div.className = "card";
+
+  div.innerHTML = `
+    <h2 class="card__title">${book.title}</h2>
+    <p class="card__author">${book.author}</p>
+    <p class="card__pages">${book.pages}</p>
+    <p class="card__read-status">${book.readStatus}</p>
+  `;
+
+  document.querySelector("#cards-container").appendChild(div);
+}
+
+myLibrary.forEach((book) => {
+  addCard(book);
+});
