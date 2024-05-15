651c4c71163cf537186a09da0d164894c1025da1
T14s <122837733+looonnng@users.noreply.github.com>
2024-05-14 23:14:05 -0500

    Add createNewID to reorder bookID after removing

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 // TODO : Add function to prevent/filter out duplicates
 // TODO : Add Remove button
 // TODO : Add Modify button??
-// TODO : Remove book from myLibrary after user click remove button2
+// TODO : Remove book from myLibrary after user click remove button
 
 const myLibrary = [];
 
 // DOM
-let cardsContainerEl = document.querySelector("#cards-container");
 const addBookBtn = document.querySelector("#addBook");
 
 function Book(title, author, pages, readStatus) {
@@ -14,6 +13,7 @@ function Book(title, author, pages, readStatus) {
   this.author = author;
   this.pages = pages;
   this.readStatus = readStatus;
+  this.id = myLibrary.length;
 }
 
 // book should be an object instance of Book constructor
@@ -21,21 +21,29 @@ function addBookToLibrary() {
   let titleField = document.querySelector("#title").value;
   let authorField = document.querySelector("#author").value;
   let pagesField = document.querySelector("#pages").value;
-  let readStatus = document.querySelector('input[name="read-status"]:checked').value;
+  let readStatus = document.querySelector(
+    'input[name="read-status"]:checked'
+  ).value;
   let newBook = new Book(titleField, authorField, pagesField, readStatus);
   myLibrary.push(newBook);
   displayBook();
+  removeBook();
 }
 
-// TODO: Simplify card.innerHTML
+// Event: Add book when user click add book button
+let cardsContainerEl = document.querySelector("#cards-container");
+document.querySelector("#new-book-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  addBookToLibrary();
+});
+
 function createCard(book) {
   const card = document.createElement("div");
   const cardModify = document.createElement("div");
   const cardContent = document.createElement("div");
   const editBtn = document.createElement("button");
   const removeBtn = document.createElement("button");
-
-  card.setAttribute("data-book-id", myLibrary.length - 1);
+  card.setAttribute("data-book-id", book.id);
   card.className = "card col";
   cardModify.className = "card-modify row";
   cardContent.className = "card-content";
@@ -58,29 +66,27 @@ function createCard(book) {
   cardsContainerEl.appendChild(card);
 }
 
-// TODO Finish Remove Button
-function removeBook() {
-  const removeBtn = document.querySelectorAll(".remove-btn");
-  removeBtn.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      const card = e.target.closest(".card");
-      const bookID = card.getAttribute("data-book-id");
-      console.log(bookID);
-      myLibrary.splice(bookID, 1); //Need to update the array after removing book
-      console.log(myLibrary);
-      card.remove();
-    });
-  });
-}
-
-// Event: Add book when user click add book button
-document.querySelector("#new-book-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  addBookToLibrary();
-});
-
-// Event: Display book
+// Event: Loop through array and display book
 function displayBook() {
   cardsContainerEl.innerHTML = ""; // Clear cardContainer DOM element to prevent duplicate
   myLibrary.forEach((book) => createCard(book));
+}
+
+function createNewID() {
+  const cards = document.querySelectorAll(".card");
+  myLibrary.forEach((book, index) => (book.id = index));
+  cards.forEach((card, index) => card.setAttribute("data-book-id", index));
+}
+
+function removeBook() {
+  const removeBtn = document.querySelectorAll(".remove-btn");
+  removeBtn.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      const card = event.target.closest(".card");
+      let bookID = card.getAttribute("data-book-id");
+      myLibrary.splice(bookID, 1); // Need to update the array after removing book
+      card.remove();
+      createNewID();
+    });
+  });
 }
